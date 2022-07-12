35d19a42fb14c7a9da512f614c0a407b4284cdbe
AnnaG2221 <99900522+AnnaG2221@users.noreply.github.com>
2022-07-12 11:27:59 -0500

    login client side verification practice

--- Practise/practise.js
+++ Practise/practise.js
@@ -1,1 +1,26 @@
-console.log('hello')
+var email = document.getElementById('email')
+var confirm = document.getElementById('confirm')
+var password = document.getElementById('password')
+var alert = document.getElementById('alert')
+
+
+var number = /^\d+$/
+var lowerCase = /^[a-z]+$/
+var upperCase = /^[A-Z]+$/
+var normal = /^\w+$/
+
+password.oninput = function(e) {
+  var value  = e.target.value
+if (number.test(value) || lowerCase.test(value) || upperCase.test(value)){
+alert.innerHTML='low security'
+alert.style.color = 'red'
+
+} else if(!normal.test(value)){
+console.log('hight')
+alert.innerHTML = 'high security'
+alert.style.color = 'green'
+} else {
+alert.innerHTML = 'middle security'
+alert.style.color = 'yellow'
+}
+}
